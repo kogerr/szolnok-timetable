c1a18b6df61e65f4e205aefe3c8917aa6d1fabc5
Zoltan Csenyi <csoltan422@gmail.com>
2018-06-08 19:45:39 +0200

    Basic REST-API implemented with express Connected to mongodb

---
+++ domain/busRoute.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var busRouteSchema  = mongoose.Schema({
+	routename: 'String',
+	busRouteLines: [{
+        startBusStop: 'String',
+        endBusStop: 'String',
+        busStops: [{
+            busStopName: 'String',
+            workDaySchedule: {
+                busArrivals: [{
+                    lowfloor: 'Boolean',
+                    arrivalHour: 'Number',
+                    arrivalMinute: 'Number'
+                }]                                
+            },
+            saturdaySchedule: {
+                busArrivals: [{
+                    lowfloor: 'Boolean',
+                    arrivalHour: 'Number',
+                    arrivalMinute: 'Number'
+                }]                                
+            },
+            sundaySchedule: {
+                busArrivals: [{
+                    lowfloor: 'Boolean',
+                    arrivalHour: 'Number',
+                    arrivalMinute: 'Number'
+                }]                                
+            }
+        }]
+	}],
+	_class: 'String'
+});
+
+mongoose.model('busRoute', busRouteSchema, 'busRoute');
