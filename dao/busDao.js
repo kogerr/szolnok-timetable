e8859ee348d0bce4d54fa10e66bf749d5edbcd9f
Zoltan Csenyi <csoltan422@gmail.com>
2018-06-19 15:18:05 +0200

    list bus stops of a bus line implemented

--- dao/busDao.js
+++ dao/busDao.js
@@ -2,11 +2,9 @@ require('./../domain/busRoute');
 const mongoose = require('mongoose');
 const busRoute =  mongoose.model('busRoute');
 
-const getBusesQuery = busRoute.find({}).select({'routename': 1, '_id': 0});
-
 exports.getBuses = () => {
     return new Promise((resolve, reject) => {
-        getBusesQuery.exec((err, data) => {
+        busRoute.find({}).select({'routename': 1, '_id': 0}).exec((err, data) => {
             if (err) {
                 reject(err);
             }
@@ -17,9 +15,33 @@ exports.getBuses = () => {
     });
 };
 
+let buildResultOfBusStopsQuery = (data, index) => {
+    let result = {};
+    result.routename = data.routename;
+    result.startStop = data.busRouteLines[index].startBusStop;
+    result.endStop = data.busRouteLines[index].endBusStop;
+    result.busStops = data.busRouteLines[index].busStops.map((element) => {
+        return element.busStopName;
+    });
+    return result;
+};
+
 
 exports.getBusStops = (busName, startStop) => {
-
+    return new Promise((resolve, reject) => {
+        busRoute.findOne({"routename": busName}, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            let result = {};
+            if (startStop && data.busRouteLines[1] && data.busRouteLines[1].startBusStop === startStop) {
+                result = buildResultOfBusStopsQuery(data, 1);
+            } else {
+                result = buildResultOfBusStopsQuery(data, 0);
+            }
+            resolve(result);
+        });
+    });
 };
 
 exports.getTimeTableOfBusStop = (busName, startStop, day) => {
--- routes/busStops.js
+++ routes/busStops.js
@@ -4,7 +4,15 @@ const busDao = require('./../dao/busDao');
 const router = express.Router();
 
 router.get('/:routename', cache(3600), (req, res) => {
-    res.send('BusStops');
+    busDao.getBusStops(req.params.routename, req.query.startStop)
+        .then((data) => {
+            res.statusCode = 200;
+            res.send(data);
+        }).catch((error) => {
+            console.log(error);
+            res.statusCode = 500;
+            res.send(error);
+        });
 });
 
 module.exports = router;
