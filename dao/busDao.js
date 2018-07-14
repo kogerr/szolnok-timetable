c96b80cf178868352a56635b1eb186fa5222f3f5
Zoltan Csenyi <cszoltan422@gmail.com>
2018-07-14 19:58:19 +0200

    Fix error loggin issue

--- dao/busDao.js
+++ dao/busDao.js
@@ -2,7 +2,7 @@ require('./../domain/busRoute');
 const mongoose = require('mongoose');
 const busRoute =  mongoose.model('busRoute');
 const logger = require('./../logger/logger');
-const error = {"message": "Bad request"};
+const errorObject = {"message": "Bad request"};
 
 
 exports.getBuses = () => {
@@ -10,10 +10,10 @@ exports.getBuses = () => {
         logger.info('Fetching all buses');
         busRoute.find({}).select({'routename': 1, '_id': 0}).exec((err, data) => {
             if (err || !data) {
-                logger.error('Can\'t fetch buses!');
-                logger.error('error: ' + error);
+                logger.error('Can\'t fetch buses! Error: [' + err + '], data: [' + data + ']');
+                logger.error('error: ' + err);
                 logger.error('data: ' + data);
-                reject(error);
+                reject(errorObject);
                 return;
             }
             resolve(data.map((element => {
@@ -40,10 +40,8 @@ exports.getBusStops = (busName, startStop) => {
         logger.info('Fetching busStops of [' + busName + '] with startStop [' + startStop + ']');
         busRoute.findOne({"routename": busName}, (err, data) => {
             if (!data || err) {
-                logger.error('Can\'t fetch busStops of bus [' + busName + '] and startStop [' + startStop + ']');
-                logger.error('error: ' + error);
-                logger.error('data: ' + data);
-                reject(error);
+                logger.error('Can\'t fetch busStops of bus [' + busName + '] and startStop [' + startStop + ']! Error: [' + err + '], data: [' + data + ']');
+                reject(errorObject);
                 return;
             }
             let result = {};
